4e96de4aa917fe468c57da5e550f44536eecfcda
fcvirtual <austin@fcvt.dev>
2023-06-13 16:15:14 -0400

    Render Movies

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -3,14 +3,35 @@ import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material'
 import { useSelector } from 'react-redux';
 
 import { useGetMoviesQuery } from '../../services/TMDB.js';
+import MovieList from '../MovieList/MovieList.jsx';
 
 const Movies = () => {
-  const { data } = useGetMoviesQuery();
+  const { data, error, isFetching } = useGetMoviesQuery();
   
-  console.log(data);
+  if(isFetching) {
+    return (
+      <Box display="flex" justifyContent="center">
+        <CircularProgress size="4rem" />
+      </Box>
+    );
+  }
+
+  if(!data.results.length) {
+    return (
+      <Box display="flex" alignItems="center" mt="20px">
+        <Typography variant="h4">
+          No movies that match that name.
+          <br />
+          Please search for something else.
+        </Typography>
+      </Box>
+    )
+  }
+
+  if(error) return 'An error has occored';
 
   return (
-    <div>Movies</div>
+    <MovieList movies={data}/>
   )
 }
 
