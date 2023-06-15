67d12ceb43742feeb23c47788cd9a01b2e14720f
fcvirtual <austin@fcvt.dev>
2023-06-15 11:14:32 -0400

    Genres

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -8,15 +8,27 @@ export const tmdbApi = createApi({
     reducerPath: 'tmdbApi',
     baseQuery: fetchBaseQuery({ baseUrl: 'https://api.themoviedb.org/3' }),
     endpoints: (builder) => ({
-        // Get Genres
-        getGenres: builder.query({
-            query: () => `genre/movie/list?api_key=${tmdbApiKey}`,
-        }),
+    //* Get Genres
+    getGenres: builder.query({
+        query: () => `genre/movie/list?api_key=${tmdbApiKey}`,
+      }),
 
-        // Get movies by [Type]
-        getMovies: builder.query({
-            query: () => `movie/popular?page=${page}&api_key=${tmdbApiKey}`,
-          }),
+    //* Get Movies by [Type]
+    getMovies: builder.query({
+        query: ({ genreIdOrCategoryName, page, searchQuery }) => {
+        //* Get Movies by Category
+        if (genreIdOrCategoryName && typeof genreIdOrCategoryName === 'string') {
+            return `movie/${genreIdOrCategoryName}?page=${page}&api_key=${tmdbApiKey}`;
+          }
+
+        //* Get Movies by Genre
+        if (genreIdOrCategoryName && typeof genreIdOrCategoryName === 'number') {
+            return `discover/movie?with_genres=${genreIdOrCategoryName}&page=${page}&api_key=${tmdbApiKey}`;
+          }
+                // Ge popular movies
+            return `movie/popular?page=${page}&api_key=${tmdbApiKey}`
+          }
+        }),
           middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(tmdbApi.middleware),
 
     }),
