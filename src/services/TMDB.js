2ba9237661486bc718d23575d3c680f6f0918f88
fcvirtual <austin@fcvt.dev>
2023-06-15 13:35:11 -0400

    Search

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -2,7 +2,6 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 // https://developer.themoviedb.org/docs
 
 const tmdbApiKey = process.env.REACT_APP_TMDB_KEY;
-const page = 1;
 
 export const tmdbApi = createApi({
     reducerPath: 'tmdbApi',
@@ -16,6 +15,11 @@ export const tmdbApi = createApi({
     //* Get Movies by [Type]
     getMovies: builder.query({
         query: ({ genreIdOrCategoryName, page, searchQuery }) => {
+        //* Get Movies by Search
+        if (searchQuery) {
+          return `/search/movie?query=${searchQuery}&page=${page}&api_key=${tmdbApiKey}`;
+        }
+
         //* Get Movies by Category
         if (genreIdOrCategoryName && typeof genreIdOrCategoryName === 'string') {
             return `movie/${genreIdOrCategoryName}?page=${page}&api_key=${tmdbApiKey}`;
