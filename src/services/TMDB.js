4e9f2f1ef632841ac782b71ae49ab12e2a1285d2
fcvirtual <austin@fcvt.dev>
2023-06-16 16:25:27 -0400

    Watch & Favorite List

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -39,6 +39,10 @@ export const tmdbApi = createApi({
         }),
 
         // Get user specific lists
+        getList: builder.query({
+          query: ({ listName, accountId, sessionId, page }) => `/account/${accountId}/${listName}?api_key=${tmdbApiKey}&session_id=${sessionId}&page=${page}`,
+        }),
+
         getRecommendations: builder.query({
           query: ({ movie_id, list }) => `/movie/${movie_id}/${list}?api_key=${tmdbApiKey}`,
         }),
@@ -58,6 +62,7 @@ export const {
     useGetGenresQuery,
     useGetMoviesQuery,
     useGetMovieQuery,
+    useGetListQuery,
     useGetRecommendationsQuery,
     useGetActorsDetailsQuery,
     useGetMoviesByActorIdQuery,
